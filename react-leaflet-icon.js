56ed3730a068b022186e1b3f9a50e4a9ea39233c
DaveSV <alb.saenz@gmail.com>
2023-04-09 12:44:23 -0600

    first commit

---
+++ react-leaflet-icon.js
@@ -0,0 +1,8 @@
+import L from 'leaflet';
+import icon from 'leaf-shadow.png';
+import iconShadow from 'leaf-shadow.png';
+
+export const MarkerIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow
+});
